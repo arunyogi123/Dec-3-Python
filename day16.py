333015cdc76dc2ddc9354d51208e24f19b341588
Arun Yogi <yogiarun290@gmail.com>
2026-01-05 12:45:13 +0545

    Error handling

---
+++ day16.py
@@ -0,0 +1,9 @@
+try:
+    # c =10
+    print("test"+adasda)
+except NameError as message:
+    print(message)
+except ZeroDivisionError as message:
+    print(message)
+except:
+    print("new type of error")
